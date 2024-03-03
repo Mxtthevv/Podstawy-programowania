f44818a184a52a1a7bb76f13bf0018175af022e2
Mateusz Dziubak <mateusz@MacBook-Pro-Mateusz.local>
2024-03-03 13:13:37 +0100

    Zadanie1.java

---
+++ L1 03.03.24/Zadanie1.java
@@ -0,0 +1,9 @@
+public class Zadanie1 {
+    public static void main(String[] args) {
+        String firstName = "Mateusz";
+        String lastName = "Dziubak";
+        int albumNumber = 50042;
+
+        System.out.println("Imie:" + " " + firstName + " " + "Nazwisko:" + " " + lastName + " " + "Nr albumu:" + albumNumber);
+    }
+}
