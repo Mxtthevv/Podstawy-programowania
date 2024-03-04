3b606ba1439e7bdfec5ae050368191b73da9bc78
Mateusz Dziubak <mateusz@MacBook-Pro-Mateusz.local>
2024-03-04 19:20:49 +0100

    Cwiczenie1.java

---
+++ L1 03.03.24/Cwiczenie1.java
@@ -0,0 +1,20 @@
+public class Cwiczenie1 {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 4;
+
+        int dodawanie = x + y;
+        System.out.println("Wynik dodawania to:" + " " + dodawanie);
+
+        int odejmowanie = x - y;
+        System.out.println("Wynik odejmowania to:" + " " + odejmowanie);
+
+        int mnozenie = x * y;
+        System.out.println("Wynik mnozenia to:" + " " + mnozenie);
+
+        int dzielenie = x / y;
+        System.out.println("Wynik dzielenia to:" + " " + dzielenie);
+    }
+}
+
+// Ćwiczenie 1) z czatu 03.03.24 //
